eae77fe9e84113dbb9f4b7ecbcb1268e0fb80d74
komasamura <biagio.chiaranda3@gmail.com>
2026-03-30 20:30:36 +0200

    Update cache version to v2 in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ⬇️ CAMBIA QUESTO NUMERO AD OGNI AGGIORNAMENTO (v1, v2, v3...)
-const CACHE_VERSION = 'v1';
+const CACHE_VERSION = 'v2';
 const CACHE_NAME = 'smartspesa-' + CACHE_VERSION;
 
 const urlsToCache = [
